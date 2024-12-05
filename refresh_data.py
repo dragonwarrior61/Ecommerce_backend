6d8f177188c9582fb3cb02bce39ac2f511191801
dragonwarrior61 <amodfired@gmail.com>
2024-12-05 10:25:05 +0200

    stock sync production test

--- refresh_data.py
+++ refresh_data.py
@@ -311,54 +311,50 @@ async def send_stock(db:AsyncSession = Depends(get_db)):
                     return
                 else:
                     logging.info(f"Find {len(db_new_orders)} new orders")
-                try:
-                    for db_new_order in db_new_orders:
-                        product_id_list = db_new_order.product_id
-                        quantity_list = db_new_order.quantity
-                        marketplace = db_new_order.order_market_place
-                        # logging.info(f"@#@#!#@#@##!@#@#@ order_id is {db_new_order.id}")
-                        for i in range(len(product_id_list)):
-                            product_id = product_id_list[i]
-                            quantity = quantity_list[i]
+                # try:
+                #     for db_new_order in db_new_orders:
+                #         product_id_list = db_new_order.product_id
+                #         quantity_list = db_new_order.quantity
+                #         marketplace = db_new_order.order_market_place
+                #         # logging.info(f"@#@#!#@#@##!@#@#@ order_id is {db_new_order.id}")
+                #         for i in range(len(product_id_list)):
+                #             product_id = product_id_list[i]
+                #             quantity = quantity_list[i]
                         
-                            result = await db.execute(select(Product).where(Product.id == product_id, Product.product_marketplace == marketplace, Product.user_id == db_new_order.user_id))
-                            db_product = result.scalars().first()
-                            if db_product is None:
-                                logging.info(f"Can't find {product_id} in {marketplace}")
-                                continue
-                            ean = db_product.ean
-                            # logging.info(f"&*&*&*&&*&*&**&ean number is {ean}")
+                #             result = await db.execute(select(Product).where(Product.id == product_id, Product.product_marketplace == marketplace, Product.user_id == db_new_order.user_id))
+                #             db_product = result.scalars().first()
+                #             if db_product is None:
+                #                 logging.info(f"Can't find {product_id} in {marketplace}")
+                #                 continue
+                #             ean = db_product.ean
+                #             # logging.info(f"&*&*&*&&*&*&**&ean number is {ean}")
 
-                            result = await db.execute(select(Internal_Product).where(Internal_Product.ean == ean))
-                            db_internal_product = result.scalars().first()
-                            if db_internal_product is None:
-                                logging.info(f"Can't find {ean}")
-                            db_internal_product.orders_stock = db_internal_product.orders_stock + quantity
-                            # logging.info(f"#$$$#$#$#$#$ Orders_stock is {db_internal_product.orders_stock}")
-                    await db.commit()
-                except Exception as e:
-                    logging.error(f"An error occurred: {e}")
-                    await db.rollback() 
+                #             result = await db.execute(select(Internal_Product).where(Internal_Product.ean == ean))
+                #             db_internal_product = result.scalars().first()
+                #             if db_internal_product is None:
+                #                 logging.info(f"Can't find {ean}")
+                #             db_internal_product.orders_stock = db_internal_product.orders_stock + quantity
+                #             # logging.info(f"#$$$#$#$#$#$ Orders_stock is {db_internal_product.orders_stock}")
+                #     await db.commit()
+                # except Exception as e:
+                #     logging.error(f"An error occurred: {e}")
+                #     await db.rollback() 
 
                 workbook = Workbook()
                 worksheet = workbook.active
                 worksheet.title = "Product Stocks"
-                worksheet.append(["EAN", "Product_code", "User_id", "Smartbill", "New Order", "Damaged", "EMAG_Stock", "Stock"])
+                worksheet.append(["EAN", "Product_code", "User_id", "Smartbill", "Damaged", "EMAG_Stock", "Stock"])
                 
                 logging.info("Sync stock")
-                result = await session.execute(select(Internal_Product))
+                result = await session.execute(select(Internal_Product).where(Internal_Product.user_id == 1))
                 db_products = result.scalars().all()
                 for product in db_products:
                     if product.smartbill_stock is None:
-                        worksheet.append([product.ean, product.product_code, product.user_id, 0, 0, 0, 0, 0])
+                        worksheet.append([product.ean, product.product_code, product.user_id, 0, 0, 0, 0])
                         continue
                     stock = product.smartbill_stock
                     smartbill_stock = stock
-                    new_order_stock = 0
                     damaged = 0
-                    if product.orders_stock:
-                        stock = stock - product.orders_stock
-                        new_order_stock = product.orders_stock
                     if product.damaged_goods:
                         stock = stock - product.damaged_goods
                         damaged = product.damaged_goods
@@ -366,39 +362,41 @@ async def send_stock(db:AsyncSession = Depends(get_db)):
                     user_id = product.user_id
                     product_code = product.product_code
                     
-                    worksheet.append([ean, product_code, user_id, smartbill_stock, new_order_stock, damaged, product.stock, stock])
+                    worksheet.append([ean, product_code, user_id, smartbill_stock, damaged, product.stock, stock])
                     
+                
+                    marketplaces = product.market_place
+                    for domain in marketplaces:
+                        if domain == "altex.ro":
+                            continue
+                            # if db_product.barcode_title == "":
+                                #     continue
+                                # post_stock_altex(marketplace, db_product.barcode_title, stock)
+                                # logging.info("post stock success in altex")
+                        result = await session.execute(select(Marketplace).where(Marketplace.marketplaceDomain == domain, Marketplace.user_id == product.user_id))
+                        marketplace = result.scalars().first()
+
+                        result = await session.execute(select(Product).where(Product.ean == ean, Product.product_marketplace == domain))
+                        db_product = result.scalars().first()
+                        product_id = db_product.id
+                                          
+                        # if marketplace.marketplaceDomain == "altex.ro":
+                        #     continue
+                        #     # if db_product.barcode_title == "":
+                        #     #     continue
+                        #     # post_stock_altex(marketplace, db_product.barcode_title, stock)
+                        #     # logging.info("post stock success in altex")
+                        
+                        product_id = int(product_id)
+                        response = await post_stock_emag(marketplace, product_id, stock)      
+                        logging.info(f"{response}") 
+                        if response == "Stock updated successfully, no content returned.":
+                            product.sync_stock_time = datetime.now()
+                        else:
+                            continue
+                    await session.commit() 
                 workbook.save("/var/www/html/invoices/stock_sync.xlsx")
                 logging.info("successfully saved stock data")
-                    # marketplaces = product.market_place
-                    # for domain in marketplaces:
-                    #     if domain == "altex.ro":
-                    #         continue
-                    #         # if db_product.barcode_title == "":
-                    #             #     continue
-                    #             # post_stock_altex(marketplace, db_product.barcode_title, stock)
-                    #             # logging.info("post stock success in altex")
-                    #     result = await session.execute(select(Marketplace).where(Marketplace.marketplaceDomain == domain, Marketplace.user_id == product.user_id))
-                    #     marketplace = result.scalars().first()
-
-                    #     result = await session.execute(select(Product).where(Product.ean == ean, Product.product_marketplace == domain))
-                    #     db_product = result.scalars().first()
-                    #     product_id = db_product.id
-                                          
-                    #     # if marketplace.marketplaceDomain == "altex.ro":
-                    #     #     continue
-                    #     #     # if db_product.barcode_title == "":
-                    #     #     #     continue
-                    #     #     # post_stock_altex(marketplace, db_product.barcode_title, stock)
-                    #     #     # logging.info("post stock success in altex")
-                        
-                    #     product_id = int(product_id)
-                    #     # response = await post_stock_emag(marketplace, product_id, stock)      
-                    #     # logging.info(f"{response}") 
-                    #     # if response == "Stock updated successfully, no content returned.":
-                    #     #     product.sync_stock_time = datetime.now()
-                    #     #     await session.commit() 
-                        
         except Exception as e:
             logging.error(f"An error occurred: {e}")
             await session.rollback()                
