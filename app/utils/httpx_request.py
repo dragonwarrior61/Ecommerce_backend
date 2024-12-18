b99871efaed9a173a4eea5ad1c0b2ff2b7c48039
phoenix19950512 <phoenix19950512@gmail.com>
2024-12-18 20:21:45 +0900

    fix: update refresh_orders

--- app/utils/httpx_request.py
+++ app/utils/httpx_request.py
@@ -5,7 +5,7 @@ from app.logfiles import log_refresh_orders
 async def send_post_request(url, headers, error_msg='', data=None, proxies=None, verify=False, params=None, json=None):
     async with httpx.AsyncClient(verify=verify, proxy=proxies) as client:
         response = await client.post(url, data=data, headers=headers, params=params, json=json)
-        log_refresh_orders(response.json())
+        log_refresh_orders(response)
         return response
 
 async def send_get_request(url, headers, error_msg='', data=None, proxies=None, verify=False, params=None, json=None):
