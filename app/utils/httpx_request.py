df51b90e37870bfae003fda1939b58bf8f362d1c
phoenix19950512 <phoenix19950512@gmail.com>
2024-12-18 20:23:31 +0900

    fix: update refresh_orders

--- app/utils/httpx_request.py
+++ app/utils/httpx_request.py
@@ -1,24 +1,21 @@
-import httpx, logging
-from fastapi import HTTPException
-from app.logfiles import log_refresh_orders
+import httpx
 
 async def send_post_request(url, headers, error_msg='', data=None, proxies=None, verify=False, params=None, json=None):
     async with httpx.AsyncClient(verify=verify, proxy=proxies) as client:
-        response = await client.post(url, data=data, headers=headers, params=params, json=json)
-        log_refresh_orders(response)
+        response = await client.post(url, data=data, headers=headers, params=params, json=json, timeout=100)
         return response
 
 async def send_get_request(url, headers, error_msg='', data=None, proxies=None, verify=False, params=None, json=None):
     async with httpx.AsyncClient(verify=verify, proxy=proxies) as client:
-        response = await client.get(url, headers=headers, params=params)
+        response = await client.get(url, headers=headers, params=params, timeout=100)
         return response
 
 async def send_patch_request(url, headers, error_msg='', data=None, proxies=None, verify=False, params=None, json=None):
     async with httpx.AsyncClient(verify=verify, proxy=proxies) as client:
-        response = await client.patch(url, data=data, headers=headers, params=params, json=json)
+        response = await client.patch(url, data=data, headers=headers, params=params, json=json, timeout=100)
         return response
 
 async def send_put_request(url, headers, error_msg='', data=None, proxies=None, verify=False, params=None, json=None):
     async with httpx.AsyncClient(verify=verify, proxy=proxies) as client:
-        response = await client.put(url, data=data, headers=headers, params=params, json=json)
+        response = await client.put(url, data=data, headers=headers, params=params, json=json, timeout=100)
         return response
